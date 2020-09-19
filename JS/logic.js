77e03af1e80d56d0819005a5ecbb1a8df129713b
1611Aryan <agupta32_be19@thapar.edu>
2020-09-20 03:47:24 +0530

    bug fixes

--- JS/logic.js
+++ JS/logic.js
@@ -10,7 +10,7 @@ let mouse = {
 };
 let i = 0;
 let j = 0;
-let increment = 1;
+let increment = 0.3;
 
 ///////////////////////////////////////////////////
 
@@ -27,7 +27,7 @@ window.addEventListener('mousemove', function (event) {
 
 //////////////////////////////////////////////////////////
 function random() {
-  return ((Math.random() - 0.5) * 16);
+  return ((Math.random() - 0.5) * 12);
 }
 //Objects
 const user1 = {
@@ -35,25 +35,25 @@ const user1 = {
   y: 0,
   z: 1,
   height: canvas.height / 5,
-  width: canvas.width / 65,
-  color: 'navy',
+  width: canvas.width / 50,
+  color: 'pink',
   level: undefined
 };
 
 const user2 = {
-  x: canvas.width * (1 - 1 / 65),
+  x: canvas.width * (1 - 1 / 50),
   y: 20,
   z: 0,
   height: canvas.height / 5,
-  width: canvas.width / 65,
+  width: canvas.width / 50,
   color: 'olive',
-  level: 0.07
+  level: 0.05
 };
 
 
 let orb = {
   x: canvas.width / 2,
-  y: (Math.random() * canvas.height / 1.4) + canvas.width / 75,
+  y: canvas.height/2,
   dx: undefined, //defined in init functio random value between [-8,-4]U[4,8]
   dy: undefined, //defined in init functio random value between [-8,-4]U[4,8]
   radius: canvas.width / 60,
@@ -81,7 +81,7 @@ function Paddle(x, y, z, width, height, color, level) {
       this.y = mouse.y - this.height / 2;
     }
     if (this.z == 0)
-      if (ball.x > canvas.width / 2) {
+      if (ball.x > canvas.width * 1 / 5) {
         this.y += (ball.y - this.y - this.height / 2) * this.level;
       }
     this.draw();
@@ -121,13 +121,6 @@ function Ball(x, y, dx, dy, radius, color) {
   if (this.dy > -3 && this.dy < 0) {
     this.dy = -4;
   }
-  if (j >= 1) {
-    if (this.dx < 0) {
-      this.dx -= increment + j / 10;
-    } else {
-      this.dx += increment + j / 10;
-    }
-  }
   this.update = () => {
     //Ball Bounds Defined
     if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
@@ -135,14 +128,23 @@ function Ball(x, y, dx, dy, radius, color) {
     }
     if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
       this.dx = -this.dx;
+
+
+      //scoring point
       if (this.x < canvas.width / 2) {
         i++;
-          
-          this.radius=0;
+
+        this.radius = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.x = canvas.width / 2;
         setTimeout(() => init(), 1000);
       } else {
         j++;
-         this.radius=0;
+        this.radius = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.x = canvas.width / 2;
         setTimeout(() => init(), 1000);
       }
     }
@@ -167,11 +169,10 @@ function init() {
   orb.dx = random();
   orb.dy = random();
   player1 = new Paddle(user1.x, user1.y, user1.z, user1.width, user1.height, user1.color, user1.level);
-
+  $('#left').css("color",user1.color);
   player2 = new Paddle(user2.x, user2.y, user2.z, user2.width, user2.height, user2.color, user2.level);
-
+$('#right').css("color",user2.color);
   ball = new Ball(orb.x, orb.y, orb.dx, orb.dy, orb.radius, orb.color);
-
 }
 
 function Divider() {
@@ -215,7 +216,7 @@ function collision(p, b) {
 function text(i = 0, j = 0) {
 
   c.font = "10rem Arial";
-  c.fillStyle = 'white';
+  c.fillStyle = 'black';
   c.fillText(j, canvas.width * (1 / 2 - 1 / 5), canvas.height / 8);
   c.fillText(i, canvas.width * (1 / 2 + 1 / 8), canvas.height / 8);
   //console.log('text');
@@ -226,7 +227,7 @@ function animate() {
   requestAnimationFrame(animate);
   let player = undefined;
   c.clearRect(0, 0, canvas.width, canvas.height)
-  divider.draw();
+  //divider.draw();
   player1.update();
   player2.update();
   ball.update();
